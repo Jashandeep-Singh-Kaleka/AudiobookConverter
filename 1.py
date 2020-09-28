dd0635fa63da0230fbb213d611c4f78e82fe0273
Jashandeep-Singh-Kaleka <jashandeepsinghkaleka@gmail.com>
2020-09-28 17:05:40 -0400

    Initial code with basic speaker, that reads through all lines

---
+++ 1.py
@@ -0,0 +1,36 @@
+# /usr/local/bin/python3 -m pip install pyttsx3
+import pyttsx3
+import PyPDF2
+from tkinter.filedialog import *
+
+# book = open('winner-stands-alone.pdf', 'rb')
+# for active selection of a book
+book = askopenfilename()
+pdfReader = PyPDF2.PdfFileReader(book)
+pages = pdfReader.numPages
+print(pages)
+
+for num in range(0, 1):
+    page = pdfReader.getPage(20)
+    text = page.extractText()
+    # text = text.lower()
+    text = text.replace('\n', '')
+    print(text)
+    speaker = pyttsx3.init()
+    speaker.say(text)
+    speaker.runAndWait()
+
+
+# Future imporvement
+# 0. Lower case the text, and remove new lines
+# 1. save last read page and line
+# 2. options to skip the foreword and index
+# 3. a default mp3 player for the audiobooks
+# 4. capabilty to move between pages
+# 5. also slow down or speed up the speed
+# 6. Manually adjust the speed by replacing punc with ,,,, to slow down
+
+
+# Thank you Programming hero for helping me listen to books
+# coz i clearly can't read
+# https://www.youtube.com/watch?v=kyZ_5cvrXJI&t=661s
